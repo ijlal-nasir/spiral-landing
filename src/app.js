f7c69ba08cb0887c197a06f4c64826b6bb8bd049
Muhammad Ijlal Nasir <ijlalfarooqi30@gmail.com>
2022-06-14 22:44:51 +0500

    add slick carousel

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 import * as bootstrap from "bootstrap";
+import $ from "jquery";
+import "slick-carousel";
+
+// const carouselContainer = document.getElementsByClassName("slick-carousel")[0];
+// console.log("carouselContainer", carouselContainer);
+
+// carouselContainer.slick({});
+
+$(".slick-carousel").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: true,
+  adaptiveHeight: true,
+});
